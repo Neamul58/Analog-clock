93e576fc12ce883859c62d0f29ad55d47cf7075e
Neamul-karim <neamulkarim1994@gmail.com>
2025-12-17 11:09:14 +0600

    first commit

--- main.js
+++ main.js
@@ -22,17 +22,29 @@ setInterval(displayTime, 1000);
 
 /*
 Note:
-===== For Hours Rotation Calculation: =====
+=====~~~~ For Hours Rotation Calculation: ~~~~=====
 12 hours = 360deg
 1 hour = (360/12)deg = 30deg
 
-==== For Minutes Hour's Bar will Forward to =====
+    ==== For Minutes Hour's Bar will Forward to =====
 60min = 30deg
-1 min = (30/60)deg
+1 min = (30/60)deg = (1/2)deg
 m min = (1/2)m deg
 
-So, h hours = 30h + (m/2)deg;
+So, h hours = 30h + (m/2);
 
+=====~~~~ For Minutes Rotation Calculation: ~~~~=====
+60 min = 360deg
+1 min = (360/60)deg = 6deg
+    ==== For Second Minute's Bar will Forward to ===== 
+60 sec = 6deg
+1 sec = (6/60)deg = (1/10)deg
+s sec = (s/10)deg
 
+So, m min = 6m + (s/10);
 
+=====~~~~ For Seconds Rotation Calculation: ~~~~=====
+60sec = 360deg
+1 sec = (360/60)deg = 6deg
+s sec = 6s
 */
