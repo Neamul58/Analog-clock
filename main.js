82e5732a7ebe7593d36d294ad91bed7583e475a5
Neamul-karim <neamulkarim1994@gmail.com>
2025-12-18 15:05:32 +0600

    first commit

--- main.js
+++ main.js
@@ -40,7 +40,25 @@
 //}
 //setInterval(displayTime, 1000);
 
+let hr = document.getElementById("hr");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
 
+function displayTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    
+    let hrRotation = 30*hh + mm/2;
+    let minRotation = 6*mm + ss/10;
+    let secRotation = 6*ss;
+    
+    hr.style.transform = `rotate(${hrRotation}deg)`;
+    min.style.transform = `rotate(${minRotation}deg)`;
+    sec.style.transform = `rotate(${secRotation}deg)`;
+}
+setInterval(displayTime, 1000);
 
 /*
 Note:
